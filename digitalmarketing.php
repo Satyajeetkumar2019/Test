e67342515cea06a60c0f379d1ec48acce03a402b
satyajeet kumar <55208293+Satyajeetkumar2019@users.noreply.github.com>
2019-09-12 00:30:12 +0530

    This is the front-end of a web.
    A website for digital marketing.

---
+++ digitalmarketing.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>digitalmarketing</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<style>
+	.container-fluid{ width:1400px;height:30px;background-color:#195B7E;
+	color:white;padding-top:10px;font-size:17px;font-family:sans-serif;
+	}
+	.b{width:1370px;height:90px;background-color:white;color:black;margin-top:-10px;margin-left:-16px;}
+	.c {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.c a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+.c a:hover,.dropdown:hover .dropbtn {
+  background-color: pink;
+}
+  </style>
+  </head>
+  <body>
+   <div class="container-fluid">
+   <div class="row">
+   <div class="col-sm-3">
+   </div>
+   <div class="col-sm-3"> <i class="fas fa-phone-square"></i>+91 7079843850</div>
+   <div class="col-sm-3">
+   </div>
+  <div class="col-sm-3">
+   <a style="color:white" href="#"><i class="fa fa-facebook-f"></i></a>
+   <a style="color:white" href="#"><i class="fa fa-twitter"></i>
+   <a style="color:white" href="#"><i class="fa fa-linkedin-in"></i>
+   <a style="color:white" href="#"><i class="fa fa-pinterest"></i>
+   </div>
+   </div>
+   <div class="row">
+   <div class="col-sm-1">
+   </div>
+   <div class="col-sm-3">
+   </div>
+   <div class="c">
+    <a href="#ABOUT US">ABOUT US</a>
+  <a href="#FACILITIES">FACILITIES</a>
+  <a href="#MEMBERSHIP">MEMBERSHIP</a>
+  <a href="#CONTACTS">CONTACT</a>
+   </div>
+   </div>
+   </div>
+   <div class="container-fluid" >
+   <div class="row" >
+   <div class="col-sm-2">
+   </div>
+   </div>
+   </div>
+   <div  class="d">
+   
+   </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
